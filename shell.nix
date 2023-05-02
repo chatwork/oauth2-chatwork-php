261f05a38188174437b1b6fa5e1bbd5d112d0589
Tomohiko Himura <eiel.hal@gmail.com>
2023-05-02 23:07:12 +0900

    chore: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+# @see https://github.com/NixOS/nixpkgs/blob/22.05/pkgs/build-support/mkshell/default.nix
+pkgs.mkShell {
+  # a list of packages to add to the shell environment
+  packages = with pkgs;
+    [pkgs.php82 pkgs.php82Packages.composer];
+  # propagate all the inputs from the given derivations
+  inputsFrom = [];
+  # support mkDerivation attrs
+}
